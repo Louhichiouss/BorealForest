67ce79d66accce93096041fa4f8ef41e24533bbd
Louhichiouss <135358285+Louhichiouss@users.noreply.github.com>
2023-06-10 22:57:14 +0100

    Update service.service.ts

--- src/app/model/service.service.ts
+++ src/app/model/service.service.ts
@@ -24,12 +24,14 @@ export class ServiceService {
     
     
   }
+    baseUrl: string="https://server.oxyboreal.com/api/"
+
 // <<<<<<< HEAD
 //   baseUrl: string="http://boreal.houssem.tn/api/"
 // =======
 //   baseUrl: string="https://boreal.hossem.tn/api/"
 // >>>>>>> a43bb19f5d252d97f5ad8bf5bc734389dda62211
-   baseUrl: string="http://server.oxyboreal.com/api/";
+  
 
    ajouterUser(user:User){
       // this.user.push(user)
@@ -228,8 +230,5 @@ export class ServiceService {
           message: string;
         }
       
-      
-            
-
       
             
