d3823116b907be06d8740d07cefdff1b7a87e2e3
Louhichiouss <135358285+Louhichiouss@users.noreply.github.com>
2023-06-16 13:05:26 +0100

    Update depense.component.ts

--- src/app/depense/depense.component.ts
+++ src/app/depense/depense.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AppComponent } from '../app.component';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
+import { ServiceService } from '../model/service.service';
 
 @Component({
   selector: 'app-depense',
@@ -6,5 +11,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./depense.component.css']
 })
 export class DepenseComponent {
+  constructor(private appcomponent:AppComponent,private http:HttpClient,private service:ServiceService
+     ,private formbuilder:FormBuilder , private router:Router, private cdr:ChangeDetectorRef , 
+    private url :ActivatedRoute
+    ) {}
 
+ngOnInit(): void {
+  this.appcomponent.hideHeaderAndFooter = true;
+}
 }
