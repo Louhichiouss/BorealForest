cf7fdb99c1785b10b4560a60133f8835f52f7317
Louhichiouss <135358285+Louhichiouss@users.noreply.github.com>
2026-06-23 09:36:21 +0100

    Update sportive.component.ts

--- src/app/sportive/sportive.component.ts
+++ src/app/sportive/sportive.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-sportive',
   templateUrl: './sportive.component.html',
   styleUrls: ['./sportive.component.css']
 })
-export class SportiveComponent {
+export class SportiveComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('in');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      threshold: 0.01,
+      rootMargin: '0px 0px -20px 0px'
+    });
+
+    document.querySelectorAll('.sr').forEach(el => {
+      observer.observe(el);
+    });
+  }
 
 }
